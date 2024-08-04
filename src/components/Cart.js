c9252c52985153b0cc60b931a3fef21b967021f6
Shubham <sk715152@gmail.com>
2024-08-04 13:31:51 +0530

    edited package.json

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,4 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { clearCart, removeItem } from "../utils/cartSlice";
 import { useNotification } from "../utils/useNotification";

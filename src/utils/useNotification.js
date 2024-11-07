4facb22a63517c724d731f1de14db90a1a991e36
Shubham <sk715152@gmail.com>
2024-11-07 13:35:34 +0530

    broken links

--- src/utils/useNotification.js
+++ src/utils/useNotification.js
@@ -22,9 +22,13 @@ export const NotificationProvider = ({ children }) => {
       {children}
       {notification && (
         <div
-          className={`flex items-center fixed top-0 right-0 font-normal text-sm py-[1rem] md:py-[1.5rem] lg:py-8 mt-2 px-2 md:px-3 lg:px-4 bg-black text-white shadow-lg transition-all duration-500 max-h-[30px] sm:max-h-[40px] lg:max-h-[50px] ease-in-out overflow-hidden ${
-            isVisible ? "max-w-[400px] opacity-100" : "max-w-0 opacity-0"
+          className={`flex items-center fixed top-0 right-0 font-normal text-sm lg:text-xl py-[1rem] md:py-[1.5rem] lg:py-8 mt-2 px-2 md:px-3 lg:px-4 bg-black text-white shadow-lg transition-all duration-500 max-h-[30px] sm:max-h-[40px] lg:max-h-[50px] overflow-hidden ${
+            isVisible ? "max-w-[300px] opacity-100" : "max-w-0 opacity-0"
           }`}
+          style={{
+            width: isVisible ? "300px" : "0",
+            opacity: isVisible ? "1" : "0",
+          }}
         >
           {notification}
         </div>
